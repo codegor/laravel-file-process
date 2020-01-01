d3440acee7bdf2e657b4928b59e352471c8e1ad5
egor <codeegor@gmail.com>
2020-01-02 01:52:23 +0200

    fix route config

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
 /** File */
-Route::group(array_merge(['namespace' => 'Codegor\Upload\Http\Controllers'], config('route_group.route_group', [])), function () {
+Route::group(array_merge(['namespace' => 'Codegor\Upload\Http\Controllers'], config('upload.route_group', [])), function () {
 	Route::get('/file/{hash}', 'File');
 });
