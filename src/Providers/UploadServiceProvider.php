7ade6f8fb561ab5af99e30b9b7e3a2604eb57e0c
egor <codeegor@gmail.com>
2020-01-03 15:32:34 +0200

    Added Validation rules inkey, indexed, starts_with_empty

--- src/Providers/UploadServiceProvider.php
+++ src/Providers/UploadServiceProvider.php
@@ -3,6 +3,7 @@ namespace Codegor\Upload\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Codegor\Upload\Store;
+use Validator;
 
 class UploadServiceProvider extends ServiceProvider {
 	/**
@@ -13,6 +14,46 @@ class UploadServiceProvider extends ServiceProvider {
 		$this->publishes([$path => config_path('upload.php')], 'config');
 		$this->loadRoutesFrom(__DIR__.'/../routes/web.php');
 		
+		
+		Validator::extend('inkey', function ($attribute, $value, $parameters, $validator) {
+			if (is_array($value) && $validator->hasRule($attribute, 'Array'))
+				return count(array_diff(array_keys($value), $parameters)) === 0;
+			else
+				return false;
+		});
+		
+		Validator::extend('index', function ($attribute, $value, $parameters, $validator) {
+			/*
+			$is_assoc = function ($array) {
+				$keys = array_keys($array);
+				return $keys !== array_keys($keys);
+			};
+			$array_type = function ($obj) {
+				$last_key = -1;
+				$type = 'index';
+				foreach ($obj as $key => $val) {
+					if (!is_int($key) || $key < 0) {
+						return 'assoc';
+					}
+					if ($key !== $last_key + 1) {
+						$type = 'sparse';
+					}
+					$last_key = $key;
+				}
+				return $type;
+			};
+			*/
+			
+			if (is_array($value) && $validator->hasRule($attribute, 'Array')) {
+				$keys = array_keys($value);
+				return $keys === array_keys($keys);
+			} else
+				return false;
+		});
+		
+		Validator::extend('starts_with_empty', function ($attribute, $value, $parameters, $validator) {
+			return (is_string($value) && trim($value) === '') || $validator->validateStartsWith($attribute, $value, $parameters);
+		});
 	}
 	
 //	public function register() {
